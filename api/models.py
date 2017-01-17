472d5950d9d39240e5f4d5b1ff881a4508d8392a
Onur Ozuduru <onur.ozuduru@gmail.com>
2017-01-17 16:09:42 +0200

    Models are created #11

--- api/models.py
+++ api/models.py
@@ -6,16 +6,55 @@
 from config import db
 
 
-class Person(db.Model):
+class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode)
-    birth_date = db.Column(db.Date)
+    company_name = db.Column(db.Unicode)
 
 
-class Article(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Unicode)
-    published_at = db.Column(db.DateTime)
-    author_id = db.Column(db.Integer, db.ForeignKey('person.id'))
-    author = db.relationship(Person, backref=db.backref('articles',
-                                                        lazy='dynamic'))
+    user_name = db.Column(db.Unicode)
+    password = db.Column(db.Unicode)
+    first_name = db.Column(db.Unicode)
+    last_name = db.Column(db.Unicode)
+    email = db.Column(db.Unicode)
+    company_id = db.Column(db.ForeignKey('company.id'))
+    company = db.relationship(Company, backref=db.backref('users', lazy='dynamic'))
+
+
+class Sign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    px = db.Column(db.Float)
+    pz = db.Column(db.Float)
+    ix = db.Column(db.Float)
+    iy = db.Column(db.Float)
+    iz = db.Column(db.Float)
+    iry = db.Column(db.Float)
+    iw = db.Column(db.Float)
+    ih = db.Column(db.Float)
+    cx = db.Column(db.Float)
+    cz = db.Column(db.Float)
+    crx = db.Column(db.Float)
+    cry = db.Column(db.Float)
+    img_url = db.Column(db.Unicode)
+    reports = db.Column(db.Unicode)
+    owner_id = db.Column(db.ForeignKey('user.id'))
+    owner = db.relationship(User, backref=db.backref('signs', lazy='dynamic'))
+    company_id = db.Column(db.ForeignKey('company.id'))
+    company = db.relationship(Company, backref=db.backref('signs', lazy='dynamic'))
+
+
+#
+# class Person(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.Unicode)
+#     birth_date = db.Column(db.Date)
+#
+#
+# class Article(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.Unicode)
+#     published_at = db.Column(db.DateTime)
+#     author_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+#     author = db.relationship(Person, backref=db.backref('articles',
+#                                                         lazy='dynamic'))
