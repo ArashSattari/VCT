8f0bc22d8cde44ef305488cd8d0b96eac6dd51a9
naukka <saniitty@student.oulu.fi>
2017-02-17 15:50:21 +0200

    db dump is added

---
+++ database/db_test.sql
@@ -0,0 +1,125 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+INSERT INTO `company` VALUES (1,'test_company'),(2,'test_company'),(3,'test_company_3');
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sign`
+--
+
+DROP TABLE IF EXISTS `sign`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sign` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `px` float DEFAULT NULL,
+  `pz` float DEFAULT NULL,
+  `ix` float DEFAULT NULL,
+  `iy` float DEFAULT NULL,
+  `iz` float DEFAULT NULL,
+  `iry` float DEFAULT NULL,
+  `iw` float DEFAULT NULL,
+  `ih` float DEFAULT NULL,
+  `cx` float DEFAULT NULL,
+  `cz` float DEFAULT NULL,
+  `crx` float DEFAULT NULL,
+  `cry` float DEFAULT NULL,
+  `img_url` varchar(250) DEFAULT NULL,
+  `reports` varchar(250) DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `owner_id` (`owner_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `sign_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `sign_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sign`
+--
+
+LOCK TABLES `sign` WRITE;
+/*!40000 ALTER TABLE `sign` DISABLE KEYS */;
+INSERT INTO `sign` VALUES (1,NULL,NULL,-73.4632,14,42.5248,2,5,5,NULL,NULL,2,2,'/data/banners/vompatti.jpg','',1,1),(2,NULL,NULL,-73.4632,14,0,2,5,5,NULL,NULL,2,2,'/data/banners/vompatti.jpg','',1,1),(3,NULL,NULL,-54.8657,14,66.674,0,5,5,NULL,NULL,0,0,'/data/banners/vompatti.jpg','',1,1),(4,NULL,NULL,-54.8657,14,66.674,0,1,1,NULL,NULL,0,0,'/data/banners/vompatti.jpg','',1,1);
+/*!40000 ALTER TABLE `sign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(250) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
+  `first_name` varchar(250) DEFAULT NULL,
+  `last_name` varchar(250) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'user_name','password','Test','Tester','test_email@asd.com',1),(3,'user_name','password','Test','Tester','test_email@asd.com',3);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-17 13:20:10
