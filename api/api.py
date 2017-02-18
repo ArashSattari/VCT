9a1c9c5f602adc9fb554404ff2b5c43cea016407
Onur Ozuduru <onur.ozuduru@gmail.com>
2017-02-18 15:15:54 +0200

    upload system, almost ready.

--- api/api.py
+++ api/api.py
@@ -17,13 +17,22 @@
 #     app.run()
 
 # Create the database tables.
-import flask_restless
-from sqlalchemy.util.langhelpers import methods_equivalent
+import os
 
-from config import db, app
+import flask_restless
+from flask import flash
+from flask import json
+from flask import redirect
+from flask import request
+from flask import url_for
+from sqlalchemy.util.langhelpers import methods_equivalent
+from werkzeug.utils import secure_filename
+
+from config import db, app, images
 from models import User, Sign, Company
 
 __API_PREFIX__ = "/api/v1.0"
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 db.create_all()
@@ -47,7 +56,37 @@ manager.create_api(Company, methods=['GET', 'POST', 'DELETE'], url_prefix=__API_
 def hello_world():
     return "It works..."
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/api/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            #filename = secure_filename(file.filename)
+            filename = images.save(file)
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return json.dumps({'filepath': '/img/'+filename})
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 # start the flask loop
 # app.run()
--- api/config.py
+++ api/config.py
@@ -2,7 +2,15 @@ import flask
 import flask_sqlalchemy
 
 # Create the Flask application and the Flask-SQLAlchemy object.
+from flask_uploads import UploadSet, IMAGES, configure_uploads, patch_request_class
+
+UPLOAD_FOLDER = '/vagrant/cst/img'
 app = flask.Flask(__name__)
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOADED_IMAGES_DEST'] = UPLOAD_FOLDER
+images = UploadSet('images', IMAGES)
+configure_uploads(app, images)
+patch_request_class(app)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ubicomp7066@localhost/test' #'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:acp123456@localhost/test' #'sqlite:////tmp/test.db'
 db = flask_sqlalchemy.SQLAlchemy(app)
