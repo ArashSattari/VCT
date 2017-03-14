e6e91358e07e93a6eceb7607d5c6405c042b99f6
Arash <arash.sattari@gmail.com>
2017-03-14 14:05:44 +0200

    before updating database

--- api/config.py
+++ api/config.py
@@ -4,7 +4,7 @@ import flask_sqlalchemy
 # Create the Flask application and the Flask-SQLAlchemy object.
 from flask_uploads import UploadSet, IMAGES, configure_uploads, patch_request_class
 
-UPLOAD_FOLDER = '/vagrant/VCT/img'
+UPLOAD_FOLDER = '~/acp1/cleaningCode/VCT/img'
 app = flask.Flask(__name__)
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['UPLOADED_IMAGES_DEST'] = UPLOAD_FOLDER
@@ -13,9 +13,7 @@ images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
 patch_request_class(app)
 app.config['DEBUG'] = True
-<<<<<<< HEAD
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:HARPdarp854@localhost/test' #'sqlite:////tmp/test.db'
-=======
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ubicomp7066@localhost/test' #'sqlite:////tmp/test.db'
->>>>>>> 9a5567eca0022f0f0bb157e3cb635b67ef8444c1
+
 db = flask_sqlalchemy.SQLAlchemy(app)
