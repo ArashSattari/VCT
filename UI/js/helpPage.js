d9a74832010e0c957caff4cbe3b8987e5e9b0527
Arash <arash.sattari@gmail.com>
2017-03-26 14:30:20 +0300

    help page typos are modified

--- UI/js/helpPage.js
+++ UI/js/helpPage.js
@@ -5,7 +5,7 @@ updateHelpPage();
 
 // Click handler for 'NEXT' button on the help page.
 $(".help .footer .nextStep").click(function(){
-    if (helpStepTracker < 3) {
+    if (helpStepTracker < 2) {
         ++helpStepTracker;
         updateHelpPage();
     }
@@ -51,9 +51,9 @@ function updateHelpPage() {
             // fill the page with related image and
             $(".help .header .text").text("Updating a Sign");
             $(".help .body img").attr("src", "UI/img/Help_2.png");
-            $(".help .body #description").html("<b style= 'font-size:15px'>To update a sign follow the bellow steps:</b>" +
+            $(".help .body #description").html("<b style= 'font-size:15px'>To update a sign follow the steps below :</b>" +
                 "<br /><br />" +
-                "<span style = 'color: #666666'>1. </span> Select one of the shops you possess, from the combo list.<br /><br/>" +
+                "<span style = 'color: #666666'>1. </span> Select your shop from the list.<br /><br/>" +
                 "<span style = 'color: #666666'>2. </span> Click on the forward and backward button to select the sign you want to update it<br /><br/>" +
                 "<span style = 'color: #666666'>3. </span> Select an image (png, jpg, etc.) from your local machine as a new sign<br /><br/>" +
                 "<span style = 'color: #666666'>4. </span> Click UPDATE button.<br /><br/>" +
@@ -67,8 +67,8 @@ function updateHelpPage() {
             // fill the page with related image and
             $(".help .header .text").text("Giving a Comment");
             $(".help .body img").attr("src", "UI/img/Help_3.png");
-            $(".help .body #description").html("<b style= 'font-size:15px'> To post a comment or give a feedback about the others' signs " +
-                "follow the bellow steps:</b>" +
+            $(".help .body #description").html("<b style= 'font-size:15px'> To post a comment or suggest changes about of any shops' signs " +
+                "follow the steps below:</b>" +
                 "<br /><br />" +
                 "<span style = 'color: #666666'>1. </span> Click on &quot;!&quot; icon<br /><br/>" +
                 "<span style = 'color: #666666'>2. </span> Write the building's number which you want to write a comment for it." +
@@ -77,13 +77,13 @@ function updateHelpPage() {
                 "<span style = 'color: #666666'>4. </span> Click on SUBMIT button.<br /><br/>" );
             break;
 
-        case 3:
-            $(".help .footer .stepsBar").attr("src", "UI/img/helpSteps_4.png");
-            // disable NEXT button
-            $(".help .footer .nextStep").css({"border": "2px solid #BBBBBB", "color": "#AAAAAA", "background-color": "#FFFFFF"});
-            $(".help .header .text").text("About Virtual Oulu");
-            $(".help .body img").attr("src", "UI/img/blank.png");
-            $(".help .body #description").html("");
-            break;
+        // case 3:
+        //     $(".help .footer .stepsBar").attr("src", "UI/img/helpSteps_4.png");
+        //     // disable NEXT button
+        //     $(".help .footer .nextStep").css({"border": "2px solid #BBBBBB", "color": "#AAAAAA", "background-color": "#FFFFFF"});
+        //     $(".help .header .text").text("About Virtual Oulu");
+        //     $(".help .body img").attr("src", "UI/img/blank.png");
+        //     $(".help .body #description").html("");
+        //     break;
     }
 }
